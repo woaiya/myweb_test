a4a319ad0dac1735f07753da354a09da8b368d68
workrk_test <1319378608@qq.com>
2019-06-21 11:10:29 +0800

    新增user表

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from login.models import UserPost
+
+
+class UserPostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'update']
+
+
+admin.site.register(UserPost, UserPostAdmin)
--- login/models.py
+++ login/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserPost(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=50)
+    state = models.IntegerField(default=0)
+    update = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name

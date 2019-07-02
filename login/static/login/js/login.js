bdf8f7427545b69034bee83c857b6657547916ab
workrk_test <1319378608@qq.com>
2019-07-02 16:37:17 +0800

    更新错误提示实现方法

--- login/static/login/js/login.js
+++ login/static/login/js/login.js
@@ -2,17 +2,17 @@ function loginForm() {
     var x = document.forms["login"]["username"].value;
     var y = document.forms["login"]["password"].value;
     if (x === ""){
-        alert("用户名不能为空！");
+        document.getElementById("usernameError").innerHTML = "用户名不能为空！";
         return false;
     }else if (y === ""){
-        alert("密码不能为空！");
+        document.getElementById("passwordError").innerHTML = "密码不能为空！";
         return false;
     }else {
         if (x.length < 6) {
-            alert("用户名不能少于6位！");
+            document.getElementById("usernameError").innerHTML = "用户名不能少于6位！";
             return false;
         }else if (y.length < 6) {
-            alert("密码不能少于6位！");
+            document.getElementById("passwordError").innerHTML = "密码不能少于6位！";
             return false;
         }
     }

957ee5d68b6b10e74f7d2e49e64980347ae59f11
workrk_test <1319378608@qq.com>
2019-07-14 14:46:21 +0800

    优化登录注册

--- login/views.py
+++ login/views.py
@@ -3,9 +3,6 @@ from django.shortcuts import render, redirect, reverse
 # Create your views here.
 
 from login.models import UserPost
-from django.http import HttpResponse
-
-from login.forms import UserAccountForm
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
@@ -18,7 +15,8 @@ def index(request):
 def ajax_login(request):
     username = request.POST['username']
     password = request.POST['password']
-    if username == 'admin' and password == '123456':
+    user = UserPost.objects.filter(username=username, password=password)
+    if user:
         msg = {"code": 200, "msg": ""}
     else:
         msg = {"code": 400, "msg": "用户名密码错误"}
@@ -30,12 +28,6 @@ def register(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
-
-        user = UserPost()
-        user.username = username
-        user.password = password
-
-        user.save()
-
+        UserPost.objects.create(username=username, password=password)
         return redirect(reverse('blog:index'))
     return render(request, 'login/registration.html')
